33c5d54e99a8153f9f24cdf82e54f7d4afe14cf3
TrucXNguyen <nguyenxuantruc30111997@gmail.com>
2022-08-08 15:22:30 +0700

    [TrucNguyen] init

---
+++ src/test/java/com/practice/aservice/AServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.aservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
